bf28e8230a93e63b12bef7bbae0fae17044af430
syrigmophonia <839486861@qq.com>
2020-05-14 22:02:42 +0800

    修改web-pom文件

---
+++ security-web/src/main/java/com/rbi/security/web/DAO/OrganizationDAO.java
@@ -0,0 +1,15 @@
+package com.rbi.security.web.DAO;
+
+import com.rbi.security.entity.web.entity.SysOrganization;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+
+@Mapper
+public interface OrganizationDAO {
+
+    @Select("SELECT * FROM sys_organization WHERE organization_name = #{rganizationName}")
+    SysOrganization queryOrganizationInfoByOrganizationName(@Param("organizationName") String rganizationName);
+
+}
