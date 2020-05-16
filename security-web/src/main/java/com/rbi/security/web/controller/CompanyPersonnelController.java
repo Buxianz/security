88bce91970b7055b9c9e9aa353b186529d586ea9
syrigmophonia <839486861@qq.com>
2020-05-16 18:12:36 +0800

    00

--- security-web/src/main/java/com/rbi/security/web/controller/CompanyPersonnelController.java
+++ security-web/src/main/java/com/rbi/security/web/controller/CompanyPersonnelController.java
@@ -30,13 +30,13 @@ public class CompanyPersonnelController {
         try {
             if (file.getOriginalFilename().indexOf("xls")>-1||file.getOriginalFilename().indexOf("xlsx")>-1) {
                 Map<String,Object> map = companyPersonnelService.excelImport(file);
-                return ResponseModel.build("","导入完成",map);
+                return ResponseModel.build("1000","导入完成",map);
             }else {
-                return ResponseModel.build("1005","文件类型错误，请使用Excel文件！");
+                return ResponseModel.build("1001","文件类型错误，请使用Excel文件！");
             }
         }catch (Exception e){
             logger.error("【公司人员信息Controller类】表格导入信息失败！，ERROR：{}",e);
-            return ResponseModel.build("1005","服务器处理失败");
+            return ResponseModel.build("1001","服务器处理失败");
         }
     }
 
@@ -45,10 +45,10 @@ public class CompanyPersonnelController {
     public ResponseModel<PageData<SysCompanyPersonnel>> queryByPage(@RequestBody JSONObject jsonObject){
         try {
             PageData<SysCompanyPersonnel> pageData = companyPersonnelService.queryByPage(jsonObject);
-            return ResponseModel.build("","",pageData);
+            return ResponseModel.build("1000","查询成功",pageData);
         }catch (Exception e){
             logger.error("【公司人员信息Controller类】分页查询公司员工信息失败，ERROR：{}",e);
-            return ResponseModel.build("", "服务器处理失败");
+            return ResponseModel.build("1001", "服务器处理失败");
         }
     }
 
@@ -57,13 +57,13 @@ public class CompanyPersonnelController {
         try {
             boolean isSuccess = companyPersonnelService.add(jsonObject);
             if (isSuccess){
-                return ResponseModel.build("","新增成功");
+                return ResponseModel.build("1000","新增成功");
             }else {
-                return ResponseModel.build("","员工号或身份证号已存在");
+                return ResponseModel.build("1001","员工号或身份证号已存在");
             }
         }catch (Exception e){
             logger.error("【公司人员信息Controller类】新增公司员工信息失败，ERROR：{}",e);
-            return ResponseModel.build("", "服务器处理失败");
+            return ResponseModel.build("1001", "服务器处理失败");
         }
     }
 
@@ -72,13 +72,13 @@ public class CompanyPersonnelController {
         try {
             boolean isSuccess = companyPersonnelService.update(jsonObject);
             if (isSuccess){
-                return ResponseModel.build("","更新成功");
+                return ResponseModel.build("1000","更新成功");
             }else {
-                return ResponseModel.build("","员工号或身份证号已存在");
+                return ResponseModel.build("1001","员工号或身份证号已存在");
             }
         }catch (Exception e){
             logger.error("【公司人员信息Controller类】更新公司员工信息失败，ERROR：{}",e);
-            return ResponseModel.build("", "服务器处理失败");
+            return ResponseModel.build("1001", "服务器处理失败");
         }
     }
 
@@ -86,10 +86,10 @@ public class CompanyPersonnelController {
     public ResponseModel<String> delete(@RequestBody JSONObject jsonObject){
         try {
             companyPersonnelService.delete(jsonObject);
-            return ResponseModel.build("","删除成功");
+            return ResponseModel.build("1000","删除成功");
         }catch (Exception e){
             logger.error("【公司人员信息Controller类】删除公司员工信息失败，ERROR：{}",e);
-            return ResponseModel.build("", "服务器处理失败");
+            return ResponseModel.build("1001", "服务器处理失败");
         }
     }
 
