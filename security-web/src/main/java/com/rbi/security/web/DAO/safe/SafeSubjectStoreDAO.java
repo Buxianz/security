3b23deb3b01689b12bac28a69dcefee8c6b847ac
muyizg <415753220@qq.com>
2020-05-27 11:38:51 +0800

    添加对题库名称的操作（CRUD）

---
+++ security-web/src/main/java/com/rbi/security/web/DAO/safe/SafeSubjectStoreDAO.java
@@ -0,0 +1,33 @@
+package com.rbi.security.web.DAO.safe;
+
+import com.rbi.security.entity.web.safe.SafeSubjectStore;
+import com.rbi.security.entity.web.system.SystemCategory;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+/**
+ * @author 杨赞格
+ */
+@Mapper
+public interface SafeSubjectStoreDAO {
+
+
+    @Select("select * from safe_subject_store_type where subject_store_name=#{subjectStoreName}")
+    SafeSubjectStore getSafeSubjectStoreByName(@Param("subjectStoreName") String subjectStoreName);
+
+    @Select("select * from safe_subject_store_type")
+    List<SafeSubjectStore> getAllSafeSubjectStore();
+
+    @Select("select subject_store_name from safe_subject_store_type where id=#{id}")
+    String getSafeSubjectStoreById(@Param("id") int id);
+
+    @Insert("INSERT INTO safe_subject_store_type (subject_store_name) VALUE (#{subjectStoreName})")
+    void insertSubjectStoreName(@Param("subjectStoreName") String subjectStoreName);
+
+    @Update("UPDATE safe_subject_store_type SET subject_store_name = #{subjectStoreName} WHERE id =#{id}")
+    void updateSubjectStoreNameById(SafeSubjectStore safeSubjectStore);
+
+    @Delete("DELETE FROM safe_subject_store_type WHERE id = #{id}")
+    void deleteSubjectStoreNameById(@Param("id") int id);
+}
